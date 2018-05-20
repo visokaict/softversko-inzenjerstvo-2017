df925073c6d565b119e8e3df5790c47deab19db8
Goran Urukalo <dev.goranurukalo@gmail.com>
2018-05-20 20:38:46 +0200

    reordered routes and added comments on frontend

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -7,17 +7,29 @@ use Illuminate\Http\Request;
 class FrontEndController extends Controller
 {
     public function gameJams(){
+        //todo
+        //move to own controller
         return view('gameJams');
     }
+
 
     //
     // games submissions views
     public function games(){
+        //todo
+        //move to own controller
         return view('gameSubmissions.games');
     }
 
     public function createGameSubmission(){
+        //todo
+        //move to own controller
         return view('gameSubmissions.createGameSubmission');
+    }
+    public function oneGameSubmission($id){
+        //todo
+        //move to own controller
+        return view('gameSubmissions.oneGameSubmission');
     }
 
 
@@ -42,11 +54,18 @@ class FrontEndController extends Controller
         return view('contactUs');
     }
 
+    
+    //
+    // profile
     public function profile() {
+        //todo
+        //move to own controller
         return view('userProfile');
     }
 
     public function editProfile() {
+        //todo
+        //move to own controller
         return view('userEdit');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,27 @@
 <?php
 
+//
+//game jams
 Route::get('/', 'FrontEndController@gameJams');
 
 //
 // game submissions
 Route::get('/games', 'FrontEndController@games');
 Route::get('/games/create', 'FrontEndController@createGameSubmission');
+Route::get('/games/{id}', 'FrontEndController@oneGameSubmission');
 
-
-Route::get('/contact-us', 'FrontEndController@contactUs');
-
+//
+//auth
 Route::get('/register', 'FrontEndController@register');
 Route::get('/login', 'FrontEndController@login');
 
-Route::get('/about', 'FrontEndController@about');
 
+//
+//profile
 Route::get('/profile', 'FrontendController@profile');
-
 Route::get('/profile/edit', 'FrontendController@editProfile');
+
+//
+//other
+Route::get('/contact-us', 'FrontEndController@contactUs');
+Route::get('/about', 'FrontEndController@about');
