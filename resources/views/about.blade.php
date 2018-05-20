3dce4367d4f7e34237e0f9a2bd356dda70815b79
simon <nikolasimonovic94@gmail.com>
2018-05-20 18:29:35 +0200

    Added About page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.frontEnd')
+
+@section('pageTitle', 'About us')
+
+@section('content')
+<div class="container">
+    <h2 class="h-bold text-center about-title">About Slam Jams project</h2>
+      <div class="container about-content">
+        <div class="row">		
+            <div class="text-center">
+              <p>
+                Uneasy barton seeing remark happen his has. 
+                Am possible offering at contempt mr distance stronger an.
+                Attachment excellence announcing or reasonable am on if indulgence. Exeter talked in agreed spirit no he unable do. Uneasy barton seeing remark happen his has.
+              </p>
+              <p>
+                Betrayed shutters in vicinity it unpacked in.
+                In so impossible appearance considered mr. Mrs him left find are good. Every great inspiration is but an experiment - though every experiment we know, is not a great inspiration.
+              </p>
+            </div>
+        </div>		
+      </div>
+</div>
+@endsection
