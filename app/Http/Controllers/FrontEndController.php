3c965cb0cdee6b768014f5f515e077b175213995
Goran Urukalo <dev.goranurukalo@gmail.com>
2018-05-26 19:54:47 +0200

    Adding dynamic navigation

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -2,52 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Models\Navigations;
 use App\Http\Models\Roles;
 use Illuminate\Http\Request;
 
 class FrontEndController extends Controller
 {
+    private $viewData;
+
+    public function __construct()
+    {
+        //todo
+        $navs = new Navigations();
+        $this->viewData['navigation'] = $navs->getAllSortedByPosition();
+    }
+
     //
     // game jams
     public function gameJams(){
         //todo
         //move to own controller
-        return view('gameJams.gameJams');
+        return view('gameJams.gameJams', $this->viewData);
     }
 
     public function oneGameJam($id){
-        return view('gameJams.oneGameJam');
+        return view('gameJams.oneGameJam', $this->viewData);
     }
 
     public function createGameJam(){
-        return view('gameJams.createGameJam');
+        return view('gameJams.createGameJam', $this->viewData);
     }
 
     public function editGameJam($id){
-        return view('gameJams.editGameJam');
+        return view('gameJams.editGameJam', $this->viewData);
     }
     //
     // games submissions views
     public function games(){
         //todo
         //move to own controller
-        return view('gameSubmissions.games');
+        return view('gameSubmissions.games', $this->viewData);
     }
 
     public function createGameSubmission(){
         //todo
         //move to own controller
-        return view('gameSubmissions.createGameSubmission');
+        return view('gameSubmissions.createGameSubmission', $this->viewData);
     }
     public function oneGameSubmission($id){
         //todo
         //move to own controller
-        return view('gameSubmissions.oneGameSubmission');
+        return view('gameSubmissions.oneGameSubmission', $this->viewData);
     }
     public function editGameSubmission($id){
         //todo
         //move to own controller
-        return view('gameSubmissions.editGameSubmission');
+        return view('gameSubmissions.editGameSubmission', $this->viewData);
     }
 
 
@@ -56,32 +66,32 @@ class FrontEndController extends Controller
     public function register(){
         $roles = new Roles();
         $userRoles = $roles->getAllAvailable();
-        return view('auth.register', [
-            'userAvailableRoles' => $userRoles
-        ]);
+
+        $this->viewData['userAvailableRoles'] = $userRoles;
+        return view('auth.register', $this->viewData);
     }
 
     public function login(){
-        return view('auth.login');
+        return view('auth.login', $this->viewData);
     }
 
 
     //
     // other
     public function about(){
-        return view('other.about');
+        return view('other.about', $this->viewData);
     }
 
     public function contactUs() {
-        return view('other.contactUs');
+        return view('other.contactUs', $this->viewData);
     }
     
     // user
     public function profile() {
-        return view('user.userProfile');
+        return view('user.userProfile', $this->viewData);
     }
 
     public function editProfile() {
-        return view('user.userEdit');
+        return view('user.userEdit', $this->viewData);
     }
 }
--- resources/views/layouts/frontEnd.blade.php
+++ resources/views/layouts/frontEnd.blade.php
@@ -9,137 +9,141 @@
 
     <title>Game Jams - @yield('pageTitle')</title>
 
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet"
+          id="bootstrap-css">
 
     <link href="{{asset('css/main.css')}}" rel="stylesheet" type="text/css">
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
-      crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+          integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+          crossorigin="anonymous">
 
-  @yield('cssfiles')
+    @yield('cssfiles')
 </head>
 
 <body>
-  <nav class="navbar navbar-inverse" style="border-radius:0">
+<nav class="navbar navbar-inverse" style="border-radius:0">
     <div class="container-fluid">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
-          aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#" style="margin-right:10px">Slam Jam</a>
-      </div>
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="active">
-            <a href="{{asset('/')}}">Game Jams
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li>
-            <a href="{{asset('/games')}}">Games</a>
-          </li>
-          <li>
-            <a href="{{asset('/about')}}">About</a>
-          </li>
-          <li>
-            <a href="{{asset('/contact-us')}}">Contact us</a>
-          </li>
-        </ul>
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1"
+                    aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#" style="margin-right:10px">Slam Jam</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav" id="navigation">
+                @isset($navigation)
+                    @foreach($navigation as $n)
+                        @if(url($n->path) == Request::url())
+                            <li class="active">
+                                <a href="{{url($n->path)}}">{{ $n->name }}
+                                    <span class="sr-only">(current)</span>
+                                </a>
+                            </li>
+                        @else
+                            <li><a href="{{ url($n->path) }}">{{ $n->name }}</a></li>
+                        @endif
+                    @endforeach
+                @endisset
 
-        <form class="navbar-form navbar-left">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search" name="q">
-            <div class="input-group-btn">
-              <button class="btn btn-default" type="submit">
-                <i class="glyphicon glyphicon-search"></i>
-              </button>
-            </div>
-          </div>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
-          <li>
-            <a href="{{asset('/login')}}">Login</a>
-          </li>
-          <li>
-            <a href="{{asset('/register')}}">Register</a>
-          </li>
-          <li>
-            <a href="{{asset('/logout')}}">Logout</a>
-          </li>
-        </ul>
-      </div>
-      <!-- /.navbar-collapse -->
+            </ul>
+
+            <form class="navbar-form navbar-left">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search" name="q">
+                    <div class="input-group-btn">
+                        <button class="btn btn-default" type="submit">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="{{asset('/login')}}">Login</a>
+                </li>
+                <li>
+                    <a href="{{asset('/register')}}">Register</a>
+                </li>
+                <li>
+                    <a href="{{asset('/logout')}}">Logout</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
     </div>
     <!-- /.container-fluid -->
-  </nav>
+</nav>
 
-  <!-- Page Content -->
-  <div class="container main-container">
-      <div class="row">
-          @if(session()->has('message'))
-              <div class="col-md-offset-1 col-md-10 alert alert-info">{{session()->get('message')}}</div>
-          @endif
-      </div>
+<!-- Page Content -->
+<div class="container main-container">
+    <div class="row">
+        @if(session()->has('message'))
+            <div class="col-md-offset-1 col-md-10 alert alert-info">{{session()->get('message')}}</div>
+        @endif
+    </div>
 
-      @yield('content')
-  </div>
-  <!-- /.container -->
+    @yield('content')
+</div>
+<!-- /.container -->
 
 <footer class="custom-footer">
-<div class="footer" id="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-                <h3> Slam Jam </h3>
-                <p>Slam Jam is a place for hosting and participating in game jams online. Anyone can start hosting a game jam immediately.</p>
+    <div class="footer" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+                    <h3> Slam Jam </h3>
+                    <p>Slam Jam is a place for hosting and participating in game jams online. Anyone can start hosting a
+                        game jam immediately.</p>
+
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+                    <h3> Contact info </h3>
+                    <ul>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+                    <h3> Social links </h3>
+                    <ul class="social">
+                        <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fab fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                    </ul>
+                </div>
 
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-                <h3> Contact info </h3>
-                <ul>
-                    <li> <a href="#"> Lorem Ipsum </a> </li>
-                    <li> <a href="#"> Lorem Ipsum </a> </li>
-                    <li> <a href="#"> Lorem Ipsum </a> </li>
-                    <li> <a href="#"> Lorem Ipsum </a> </li>
-                </ul>
-            </div>
-
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-                <h3> Social links </h3>
-                <ul class="social">
-                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fab fa-google-plus"></i></a> </li>
-                    <li><a href="#"><i class="fab fa-pinterest"></i></a> </li>
-                    <li><a href="#"><i class="fab fa-youtube"></i></a> </li>
-                </ul>
-            </div>
-
+            <!--/.row-->
         </div>
-        <!--/.row-->
+        <!--/.container-->
     </div>
-    <!--/.container-->
-</div>
-<!--/.footer-->
+    <!--/.footer-->
 
-<div class="footer-bottom">
-    <div class="container">
-        <p class="pull-left"> Copyright © Slam Jam 2018. All right reserved. </p>
+    <div class="footer-bottom">
+        <div class="container">
+            <p class="pull-left"> Copyright © Slam Jam 2018. All right reserved. </p>
+        </div>
     </div>
-</div>
-<!--/.footer-bottom-->
+    <!--/.footer-bottom-->
 </footer>
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- Bootstrap core JavaScript -->
+<script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
-  @yield('jsfiles')
+@yield('jsfiles')
 
 </body>
 
