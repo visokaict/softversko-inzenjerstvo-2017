3720d6c6bd90d2067ddefdb3dc626001dfa2e7f8
Goran Urukalo <dev.goranurukalo@gmail.com>
2018-05-26 14:41:40 +0200

    Added needed files for interface and registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,40 +3,41 @@
 @section('pageTitle', 'Registration')
 
 @section('content')
-<div class="auth-box-body">
-    <p class="auth-box-msg auth-title">Register a new membership</p>
-    <form action="../../index.html" method="post">
-      <div class="form-group has-feedback">
-        <input type="text" class="form-control" placeholder="Username">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Retype password">
-        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <!-- /.col -->
-        <div class="col-xs-8">
-            <label>
-              <a href="{{asset('/login')}}" style="font-weight:normal">I already have an account</a>
-            </label>
-        </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-            <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
+    <div class="auth-box-body">
+        <p class="auth-box-msg auth-title">Register a new membership</p>
+        <form action="{{asset('/register')}}" method="post">
+            {{csrf_field()}}
+            <div class="form-group has-feedback">
+                <input type="text" class="form-control" placeholder="Username" name="tbUsername">
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="email" class="form-control" placeholder="Email" name="tbEmail">
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" class="form-control" placeholder="Password" name="tbPassword">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" class="form-control" placeholder="Retype password" name="tbConfirmPassword">
+                <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+            </div>
+            <div class="row">
+                <!-- /.col -->
+                <div class="col-xs-8">
+                    <label>
+                        <a href="{{asset('/login')}}" style="font-weight:normal">I already have an account</a>
+                    </label>
+                </div>
+                <!-- /.col -->
+                <div class="col-xs-4">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat" name="btnSubmit">Register</button>
+                </div>
+                <!-- /.col -->
+            </div>
+        </form>
 
 
-  </div>
+    </div>
 @endsection
