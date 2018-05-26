a5021583714b1bd4e0944a66fdc5e6950c8bbaba
Goran Urukalo <dev.goranurukalo@gmail.com>
2018-05-26 15:32:07 +0200

    Adding users model so we can both work on it

---
+++ app/Http/Models/Users.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Models;
+
+
+class Users extends Generic
+{
+    public function __construct()
+    {
+        parent::__construct('users', 'idUser');
+    }
+
+    //todo
+}
