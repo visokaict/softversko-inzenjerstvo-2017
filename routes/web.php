584841aec871cb16fdf41267b0cde5ca92640819
simon <nikolasimonovic94@gmail.com>
2018-05-26 21:29:32 +0200

    Fixed routes

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 //
 // game submissions
 Route::get('/games', 'FrontEndController@games');
-Route::get('/games/create', 'FrontEndController@createGameSubmission')->middleware('isLoggedIn, isJamDeveloper');
+Route::get('/games/create', 'FrontEndController@createGameSubmission')->middleware('isLoggedIn', 'isJamDeveloper');
 Route::get('/games/{id}', 'FrontEndController@oneGameSubmission');
-Route::get('/games/{id}/edit', 'FrontEndController@editGameSubmission')->middleware('isLoggedIn, isJamDeveloper');
+Route::get('/games/{id}/edit', 'FrontEndController@editGameSubmission')->middleware('isLoggedIn', 'isJamDeveloper');
 
 //
 // game jams
 Route::get('/', 'FrontEndController@gameJams');
-Route::get('/game-jams/create', 'FrontEndController@createGameJam')->middleware('isLoggedIn, isJamMaker');
+Route::get('/game-jams/create', 'FrontEndController@createGameJam')->middleware('isLoggedIn', 'isJamMaker');
 Route::get('/game-jams/{id}', 'FrontEndController@oneGameJam');
-Route::get('/game-jams/{id}/edit', 'FrontEndController@editGameJam')->middleware('isLoggedIn, isJamMaker');
+Route::get('/game-jams/{id}/edit', 'FrontEndController@editGameJam')->middleware('isLoggedIn', 'isJamMaker');
 
-Route::post('/game-jams/create', 'GameJamController@insert')->middleware('isLoggedIn, isJamMaker');
+Route::post('/game-jams/create', 'GameJamController@insert')->middleware('isLoggedIn', 'isJamMaker');
 
 Route::get('/contact-us', 'FrontEndController@contactUs');
 
