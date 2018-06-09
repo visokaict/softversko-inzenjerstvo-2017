dfd4d0232292678b127514b5faddf4851669adef
Goran Urukalo <dev.goranurukalo@gmail.com>
2018-06-09 22:18:53 +0200

    Adding check if there is data to show, game jam page

--- resources/views/ajax/loadGameJamsInProgress.blade.php
+++ resources/views/ajax/loadGameJamsInProgress.blade.php
@@ -1,32 +1,38 @@
 <div class="game-jams-content">
     <div id="load" style="position: relative;">
-        @foreach($inProgressGameJams as $gameJam)
-            <div class="col-lg-4 col-sm-6 portfolio-item">
-                <div class="card h-100">
-                    <a href="{{asset('game-jams/'. $gameJam->idGameJam)}}"><img class="card-img-top" style="height: 190px; object-fit: cover;" src="{{asset($gameJam->path)}}" alt="{{$gameJam->alt}}"></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="{{asset('game-jams/'. $gameJam->idGameJam)}}">{{ $gameJam->title }}</a>
-                        </h4>
-                        <h6 class="card-subtitle mb-2 text-muted">Hosted by: <a href="{{asset('user/'. $gameJam->username)}}">{{ $gameJam->username }}</a></h6>
-                        <p class="margin-bottom-5">Submissions closes in: <span>
-                            @if(($gameJam->endDate - time()) / 3600 > 24)
-                                {{ floor(($gameJam->endDate - time()) / 3600 / 24) . " days" }}
-                            @else
-                                {{ ceil(($gameJam->endDate - time()) / 3600) . " hr" }}
-                            @endif
-                        </span></p>
-                        <div class="progress">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0"
-                                    aria-valuemax="100" style="width: {{ floor(((time() - $gameJam->startDate) / ($gameJam->endDate - $gameJam->startDate)) * 100) }}%;">
+        @if(count($inProgressGameJams))
+
+            @foreach($inProgressGameJams as $gameJam)
+                <div class="col-lg-4 col-sm-6 portfolio-item">
+                    <div class="card h-100">
+                        <a href="{{asset('game-jams/'. $gameJam->idGameJam)}}"><img class="card-img-top" style="height: 190px; object-fit: cover;" src="{{asset($gameJam->path)}}" alt="{{$gameJam->alt}}"></a>
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                <a href="{{asset('game-jams/'. $gameJam->idGameJam)}}">{{ $gameJam->title }}</a>
+                            </h4>
+                            <h6 class="card-subtitle mb-2 text-muted">Hosted by: <a href="{{asset('user/'. $gameJam->username)}}">{{ $gameJam->username }}</a></h6>
+                            <p class="margin-bottom-5">Submissions closes in: <span>
+                                @if(($gameJam->endDate - time()) / 3600 > 24)
+                                    {{ floor(($gameJam->endDate - time()) / 3600 / 24) . " days" }}
+                                @else
+                                    {{ ceil(($gameJam->endDate - time()) / 3600) . " hr" }}
+                                @endif
+                            </span></p>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0"
+                                        aria-valuemax="100" style="width: {{ floor(((time() - $gameJam->startDate) / ($gameJam->endDate - $gameJam->startDate)) * 100) }}%;">
+                                </div>
                             </div>
+                            <p class="card-subtitle mb-2 text-muted p-joind-submissions">X<span> joined</span> Y <span>submissions</span>
+                            </p>
                         </div>
-                        <p class="card-subtitle mb-2 text-muted p-joind-submissions">X<span> joined</span> Y <span>submissions</span>
-                        </p>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+
+        @else
+            <i>There is currently no game jams in progress</i>
+        @endif
     </div>
     <div class="clearfix"></div>
 </div>
