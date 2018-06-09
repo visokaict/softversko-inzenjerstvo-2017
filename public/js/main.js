57fd35e1a1027321895eff4af456e0e228f36ffc
Goran Urukalo <dev.goranurukalo@gmail.com>
2018-06-09 14:39:02 +0200

    Adding game jam javascript function and moving logic to main.js

--- public/js/main.js
+++ public/js/main.js
@@ -85,6 +85,53 @@ slamjam.games = (function(){
 **/
 slamjam.gameJam = (function(){
     //todo
+
+    function _initChart() {
+        // create a dataset with items
+        // note that months are zero-based in the JavaScript Date object, so month 3 is April
+        var items = new vis.DataSet([
+            {id: 0, content: 'item 0', start: new Date(2018, 5, 1), end: new Date(2018, 5, 12), link: "as"},
+            {id: 1, content: 'item 1', start: new Date(2018, 5, 6), end: new Date(2018, 5, 16), link: "as"},
+            {id: 2, content: 'item 2', start: new Date(2018, 5, 18), end: new Date(2018, 5, 22), link: "as"},
+            {id: 3, content: 'item 2', start: new Date(2018, 5, 16), end: new Date(2018, 5, 24), link: "as"},
+        ]);
+
+        // create visualization
+        var container = document.getElementById('visualization');
+        var options = {
+            maxHeight: 400,
+            editable: false,
+            clickToUse: false,
+            zoomable: false,
+            selectable: false,
+            start: new Date(2018, 5, 14),
+            template: function (data, x, y) {
+                return `<a href='${data.link}'>${data.content}</a>`;
+            }
+        };
+
+        var timeline = new vis.Timeline(container);
+        timeline.setOptions(options);
+        //timeline.setGroups(groups);
+        timeline.setItems(items);
+
+
+        //
+        // DOCS: http://visjs.org/docs/timeline/
+        //
+        //dodati Start , End
+        //dodaj boje neke
+
+    }
+
+    function _initGameJamItems() {
+
+    }
+
+    return {
+        initChart: _initChart,
+        initGameJamItems: _initGameJamItems,
+    }
 })();
 
 /*
