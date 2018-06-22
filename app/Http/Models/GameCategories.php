a537bfffcbd5935ec8ea7b43ae38fb62be881e9b
simon <nikolasimonovic94@gmail.com>
2018-06-22 18:24:48 +0200

    Admin edit/insert game categories

---
+++ app/Http/Models/GameCategories.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Models;
+
+class GameCategories extends Generic
+{
+    public function __construct()
+    {
+        parent::__construct('gamecategories', 'idGameCategory');
+    }
+}
